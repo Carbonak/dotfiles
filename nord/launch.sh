0b3a05c1fc80afaa6f8b65c50bea2de32b81945b
michael <michael.l.blakeman@gmail.com>
2021-08-13 20:54:54 -0700

    updated files

--- nord/launch.sh
+++ nord/launch.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Wait until the porcesses have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done 
 
-# Launch polybar
-#polybar main2 -c $(dirname $0)/config.ini &
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main -c ~/dotfiles/nord/config.ini &
-  done
-else
-  polybar --reload example &
-fi
+polybar main -c $(dirname $0)/config.ini & 
