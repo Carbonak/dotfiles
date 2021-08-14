b9049403a88f6d2bad1498764cdc37cb820576bb
michael <michael.l.blakeman@gmail.com>
2021-08-13 21:26:46 -0700

    updated files

--- nord/launch.sh
+++ nord/launch.sh
@@ -7,4 +7,12 @@ killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done 
 
 # Launch Polybar
-polybar main -c $(dirname $0)/config.ini & 
+#polybar main -c $(dirname $0)/config.ini & 
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main -c $(dirname $0)/config.ini &
+  done
+else
+  polybar --reload main -c $(dirname $0)/config.ini &
+fi
