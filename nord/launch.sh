7ef96eaa97c577a616befa13af0b071d8de061f0
michael <michael.l.blakeman@gmail.com>
2021-08-13 20:55:49 -0700

    updated files

--- nord/launch.sh
+++ nord/launch.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# Kill all current instances of polybar
 killall polybar
 
 # Wait until the porcesses have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done 
 
+# Launch Polybar
 polybar main -c $(dirname $0)/config.ini & 
